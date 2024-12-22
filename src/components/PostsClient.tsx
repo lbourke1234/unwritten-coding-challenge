c33ec8007649f4120d3dd1d9ca6a7046291596b1
lbourke1234 <lbourke1234@gmail.com>
2024-12-22 16:32:57 +0000

    framer motion added

--- src/components/PostsClient.tsx
+++ src/components/PostsClient.tsx
@@ -2,6 +2,7 @@
 
 import { Post } from "@/types/types";
 import Error from "./Error";
+import { motion } from "framer-motion";
 
 type PostsClientProps = {
   posts: Post[];
@@ -17,13 +18,17 @@ export default function PostsClient({ posts, error }: PostsClientProps) {
     <div className="flex flex-col items-center p-8 container mx-auto">
       <div className="flex flex-wrap justify-center gap-6 w-full mt-6">
         {posts.slice(0, 20).map((post) => (
-          <div
+          <motion.div
+            initial={{ opacity: 0, scale: 0.95 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.3, ease: "easeOut" }}
+            whileHover={{ scale: 1.02 }}
             key={post.id}
             className="flex flex-col justify-between p-4 bg-black text-white rounded shadow-lg w-72"
           >
             <h2 className="text-lg font-bold mb-2">{post.title}</h2>
             <p>{post.body}</p>
-          </div>
+          </motion.div>
         ))}
       </div>
     </div>
